2ec4595eba54d3de446c3b949aa9a7442fe1cee7
swedev99 <swedevops99@gmail.com>
2023-06-04 23:43:37 +0530

    code dry

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "tags" {}
+variable "env" {}
+variable "subnets" {}
+variable "name" {
+  default = "docdb"
+}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "engine_version" {}
+variable "kms_arn" {}
+variable "port_no" {
+  default = 27017
+}
